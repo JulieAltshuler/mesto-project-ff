34aadbe8f702f918a78203c298a07e41ff2b1db6
Julie Altshuler <julie.altshuler@gmail.com>
2024-03-18 12:49:24 +0300

    реализован рендер карточек из массива и кнопка удаления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+const cardsList = document.querySelector('.places__list'); // определяем контейнер, куда добавим карточки
+const cardTemplate = document.querySelector('#card-template').content; // получаем содержимое тэга template
 
-// @todo: DOM узлы
+function createCard (cardData, cardDeleter) { // функция добавления карточки, параметры - информация одной карточки и функция удаления карточки
+    const cardsUser = cardTemplate.cloneNode(true); // создает новый экземпляр шаблона карточки
+    const cardImage = cardsUser.querySelector('.card__image'); // определяем класс, отвечающий за отрисовку изображения карточки
+    const cardTitle = cardsUser.querySelector('.card__title'); // определяем класс, отвечающий за отрисовку наименования карточки
+    cardImage.src = cardData.link; // указываем взаимосвязь константы с классом и поля массива
+    cardTitle.textContent = cardData.name; // указываем взаимосвязь константы с классом и поля массива
+    
+    const cardDeleterIcon = cardsUser.querySelector('.card__delete-button'); // определяем класс, отвечающий за удаление карточки
+    cardDeleterIcon.addEventListener('click', function() { // добавляем обработчик клика на кнопку удаления
+        cardDeleter(); // вызываем функцию удаления карточки
+    })
+    return cardsUser; // возвращаем готовую карточку как результат функции
+};
 
-// @todo: Функция создания карточки
+function cardDeleter() { // функция удаления карточки
+    const card = document.querySelector('.places__item'); // определяем класс, отвечающий за отрисовку карточки
+    card.remove(); // удаляем карточку
+}
 
-// @todo: Функция удаления карточки
+initialCards.forEach(function (cardData) { // перебираем с помощью цикла для каждой карточки массива
+    const cardsUser = createCard(cardData, cardDeleter); // создаем карточку функцией createCard, используя данные этой карточки и функцию удаления. добавляем ее в переменную
+    cardsList.append(cardsUser); // добавляем карточку в контейнер cardsList, отображается на стратнице
+})
 
-// @todo: Вывести карточки на страницу
+
+
+
